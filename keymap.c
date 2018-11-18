3546e3bc24b06bf61d9f90afdbfd352125672f96
mikoto2000 <mikoto2000@gmail.com>
2018-11-19 06:11:30 +0900

    ベースの矢印キーの位置を修正。

--- keymap.c
+++ keymap.c
@@ -65,15 +65,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   B  |   N  |   M  |  ,<  |  .>  |  /?  |  \_  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| Alt  | ESC  | GUI  |Lower | Mhen |Space |Space | Henk |Raise | Left |  Up  | Down |Right |
+   * |Adjust| Alt  | ESC  | GUI  |Lower | Mhen |Space |Space | Henk |Raise | Left | Down | Up   |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_BASE] = LAYOUT( \
-    KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    JP_AT,   \
-    KC_LCTL, KC_A,    KC_S,   KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, JP_COLN, \
-    KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    KC_N,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, JP_BSLS,  \
-    ML_ADJ,  KC_LALT, KC_ESC, KC_LGUI, ML_LOW, JP_MHEN, KC_SPC,  KC_SPC,  JP_HENK, ML_RAI, KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT  \
+    KC_ESC,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    JP_AT,   \
+    KC_LCTL, KC_A,    KC_S,   KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, JP_COLN, \
+    KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,   KC_B,    KC_N,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, JP_BSLS,  \
+    ML_ADJ,  KC_LALT, KC_ESC, KC_LGUI, ML_LOW, JP_MHEN, KC_SPC,  KC_SPC,  JP_HENK, ML_RAI, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT  \
     ),
 
   /* Lower JIS Normal
